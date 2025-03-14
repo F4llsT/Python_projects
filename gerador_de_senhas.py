1ef29b892e28e50d9ccb202b9f748d9618971265
Arthur Lopes Libonati <arthurlibonati5@gmail.com>
2025-03-14 20:29:33 -0300

    Create gerador_de_senhas.py

---
+++ gerador_de_senhas.py
@@ -0,0 +1,17 @@
+import random #importando a biblioteca de randomização
+
+print("Bem-vindo ao seu gerador de senha pessoal!") #título inicial
+
+caracteres = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ@#$_&*()/1234567890" 
+#alfabeto maiúsculo e minúsculo, além de alguns símbolos e números para a randomização
+
+numero = int(input("Qual é o número de senhas que você deseja criar? "))
+quantidade = int(input("Qual é o número de caracteres que você deseja para cada senha? "))
+#requisição de valores sobre o número de senhas que o usuário deseja criar  
+#e o número de caracteres destas.
+
+print("\nAqui estão suas novas senhas:\n") #requisição de saída do código
+
+for _ in range(numero): 
+    senha = "".join(random.choice(caracteres) for _ in range(quantidade))
+    print(senha) #saída resultante do código, com o número de senhas e caracteres requerido.
